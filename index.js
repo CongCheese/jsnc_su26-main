5c7a5b9dc9efe38cf9906cfecc78b2c98de449a8
CongNguyenChi <chesecong@gmail.com>
2026-08-05 09:52:17 +0700

    bai1

---
+++ index.js
@@ -0,0 +1,22 @@
+let Name = "Nguyễn Chí Công";
+name = "Nguyễn Chí Công 1";
+let Local = "Mỹ Đình";
+let Email = "chesecong@gmail.com";
+const birthYear = 2004;
+let age = 22;
+
+console.log (Name);
+console.log (Local);
+console.log (Email);
+console.log (birthYear);
+
+let isAdmim = true;
+
+const Book = {
+    id:1,
+    name: "Java",
+    price: 20000
+};
+console.log(Book);
+
+alert("jsnc");
