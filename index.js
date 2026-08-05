623e8bc88f5f5b3c46424480bab7355c23e52252
CongNguyenChi <chesecong@gmail.com>
2026-08-05 15:52:38 +0700

    ff

--- index.js
+++ index.js
@@ -16,14 +16,29 @@ let isAdmim = true;
 
 const book = {
     id:1,
-    bookName: "Java",
+    bookName: "One Piece",
     price: 20000
 };
 console.log(book);
 console.log(book.bookName);
+document.getElementById("book").innerHTML=`
+<h2> ${book.bookName}</h2>
+`;
 // alert("jsnc");
 
 const h1 = document.getElementById("title");
 console.log(h1);
 const title = "danh sách sản phẩm"
 h1.innerHTML = `danh sách sản phẩm ${title}`
+
+
+
+const student = {
+    studentName:"Công",
+    studentAge: 22,
+    studentSchool: "FPY"
+};
+console.log(student);
+console.log(student.studentName);
+document.getElementById("student").innerHTML=`
+<h2> ${student.studentName}</h2>`
