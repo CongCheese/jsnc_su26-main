6f0c1d5d608b2b8f8851f9612b17e7a7d80e0452
CongNguyenChi <chesecong@gmail.com>
2026-08-12 15:20:46 +0700

    editv2

--- edit.js
+++ edit.js
@@ -1,12 +1,19 @@
-document.getElementById("form-add").addEventListener("submit",(e)=>{
+console.log("edit");
+const params = new URLSearchParams(window.location.search);
+console.log(params);
+const id = params.get("id");
+console.log(id);
+document.getElementById("form-edit").addEventListener("submit",(e)=>{
     e.preventDefault();
     const name = document.getElementById("title").value;
     const price = document.getElementById("price").value;
-     axios.post("http://localhost:3000/products",{
+     axios.put(`http://localhost:3000/products/${id}`,{
         name: name,
         price: price,
     })
     .then(()=>{
-        alert("them ok");
+        alert("cập nhật ok");
+    }).catch((err)=>{
+        alert("cập nhật không ok");
     })
 });
