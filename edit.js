5b0036401e45ba5d39d69127247185081941e00e
CongNguyenChi <chesecong@gmail.com>
2026-08-13 14:52:40 +0700

    edit 3

--- edit.js
+++ edit.js
@@ -3,6 +3,16 @@ const params = new URLSearchParams(window.location.search);
 console.log(params);
 const id = params.get("id");
 console.log(id);
+if (id){
+    axios.get(`http://localhost:3000/products/${id}`)
+    .then((res)=>{
+        const product = res.data;
+        document.getElementById("title").value = product.name;
+        document.getElementById("price").value = product.price;
+    }).catch((err)=>{
+        alert("lấy dữ liệu không ok");
+    })
+}
 document.getElementById("form-edit").addEventListener("submit",(e)=>{
     e.preventDefault();
     const name = document.getElementById("title").value;
